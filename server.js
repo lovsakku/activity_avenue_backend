e6936036b75a63cdf58af65f87c82b4dcc1b4b99
lovsakku <sakinaafs@gmail.com>
2024-12-13 15:39:49 +0400

    check

--- server.js
+++ server.js
@@ -1,153 +1,136 @@
 const express = require("express");
-const { MongoClient, ObjectId } = require("mongodb");
-const cors = require("cors");
-var path = require("path");
-var fs = require("fs");
 const app = express();
+const { MongoClient, ObjectID } = require("mongodb");
 
+app.use(express.json());
+app.set("port", 3000);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Origin",
-      "Accept",
-      "X-Requested-With",
-    ],
-    credentials: true,
-  })
-);
-
-const url ="mongodb+srv://sakina:fullstack@cluster0.4m3fb.mongodb.net/";
-const dbName = "Webstore";
-let db;
-
-app.use(express.json()); // middleware to use json
-
-app.use(function (req, res, next) {
-  // logging middleware
-  console.log("Request IP: " + req.url);
-  console.log("Request Body: " + JSON.stringify(req.body));
-  console.log("Request Query Params: " + JSON.stringify(req.query));
-  console.log("Request date: " + new Date());
   next();
 });
 
-app.use(function (req, res, next) {
-  // static image file middleware
-  var filePath = path.join(__dirname, "static", req.url);
-  fs.stat(filePath, function (err, fileInfo) {
+//Function to log the current time
+function logCurrentTime() {
+  const time = new Date();
+  const options = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  return time.toLocaleDateString("en-US", options);
+}
+
+let db;
+MongoClient.connect(
+  "mongodb+srv://sakina:fullstack@cluster0.4m3fb.mongodb.net/",
+  (err, client) => {
     if (err) {
-      next();
-      return;
+      console.error("Failed to connect to MongoDB:", err);
+      process.exit(1);
     }
-    if (fileInfo.isFile()) res.sendFile(filePath);
-    else next();
+    db = client.db("School__Activities");
+    console.log("Connected to MongoDB");
+  }
+);
+
+
+app.get("/", (req, res) => {
+  res.send("Select a collection, e.g., /collection/messages");
+});
+
+
+app.param("collectionName", (req, res, next, collectionName) => {
+  req.collection = db.collection(collectionName);
+  return next();
+});
+
+
+app.get("/collection/:collectionName", (req, res, next) => {
+  req.collection.find({}).toArray((e, results) => {
+    if (e) return next(e);
+    res.send(results);
   });
 });
 
-MongoClient.connect(url) 
-  .then((client) => {
-    console.log("Connected to MongoDB");
-    db = client.db(dbName);
 
-    app.get("/", (req, res) => {
-      res.send("Hello, World! MongoDB connected.");
-    });
-
-    app.post("/collection/:collectionName/addMany", async (req, res) => {
-      try {
-        let collection = db.collection(req.params.collectionName);
-        const result = await collection.insertMany(req.body);
-        return res.send(result);
-      } catch (err) {
-        console.error("Error adding multi records to db:", err);
-        return res.status(500).send("Error adding multi records to db");
-      }
-    });
-
-    app.post("/collection/:collectionName/addOne", async (req, res) => {
-      try {
-        let collection = db.collection(req.params.collectionName);
-        const result = await collection.insertOne(req.body);
-        return res.send(result);
-      } catch (err) {
-        console.error("Error adding single record to db:", err);
-        return res.status(500).send("Error adding single record to db");
-      }
-    });
-
-    app.get("/collection/products", async (req, res) => {
-
-      try {
-        const collection = db.collection(req.params.collectionName);
-        const items = await collection.find({}).toArray();
-        return res.json(items);
-      } catch (err) {
-        console.error("Error fetching items:", err);
-        return res.status(500).send("Error fetching items");
-      }
-    });
-
-    app.put("/collection/:collectionName/update/:id", async (req, res) => {
-      
-        try {  
-          let collection = db.collection(req.params.collectionName);
-          const filter = { _id: new ObjectId(req.params.id) };
-          const updateDocument = { $set: req.body };
-          const result = await collection.updateOne(filter, updateDocument);
-          return res.send(result);
-        } catch (err) {
-          console.error("Error while trying to update record:", err);
-          return res.status(500).send("Error while trying to update record");
-        }
-      });
-
-    app.get("/collection/products/search", async (req, res) => {
-      
-      try {
-        const collection = db.collection(req.params.collectionName);
-        const query = req.query.value;
-        if (!query) {
-          return res.status(400).json({ error: "Query parameter is required" });
-        }
-        const searchQuery = {};
-        if (!isNaN(query)) {
-          const queryNumber = parseInt(query);
-          searchQuery.$or = [
-            { price: queryNumber },
-            { availableInventory: queryNumber },
-            { rating: queryNumber },
-          ];
-        } else {
-          searchQuery.$or = [
-            { title: { $regex: query, $options: "i" } }, 
-            { location: { $regex: query, $options: "i" } },
-            { description: { $regex: query, $options: "i" } },
-          ];
-        }
-        const items = await collection.find(searchQuery).toArray();
-        return res.json(items);
-      } catch (err) {
-        console.error("Error fetching items:", err);
-        return res.status(500).send("Error fetching items");
-      }
-    });
-
-
-    app.use(function (req, res) {
-      res.status(404);
-      res.send("Path not found!");
-    });
-  })
-  .catch((err) => {
-    console.error("Cannot connecting to MongoDB:", err);
+app.post("/collection/:collectionName", (req, res, next) => {
+  req.collection.insertOne(req.body, (e, result) => {
+    if (e) return next(e);
+    res.send(result.ops[0]);
   });
-const port = process.env.PORT||3001;
+});
+
+
+app.get("/collection/:collectionName/:id", (req, res, next) => {
+  req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+    if (e) return next(e);
+    res.send(result);
+  });
+});
+
+
+app.put("/collection/:collectionName/:id", (req, res, next) => {
+  req.collection.updateOne(
+    { _id: new ObjectID(req.params.id) },
+    { $set: req.body },
+    { safe: true },
+    (e, result) => {
+      if (e) return next(e);
+      res.send(result.matchedCount === 1 ? { msg: "success" } : { msg: "error" });
+    }
+  );
+});
+
+
+app.delete("/collection/:collectionName/:id", (req, res, next) => {
+  req.collection.deleteOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+    if (e) return next(e);
+    res.send(result.deletedCount === 1 ? { msg: "success" } : { msg: "error" });
+  });
+});
+
+
+app.post("/place-order", (req, res) => {
+  const ordersCollection = db.collection("orders");
+  const order = req.body;
+
+  if (
+    !order.firstName ||
+    !order.lastName ||
+    !order.address ||
+    !order.city ||
+    !order.state ||
+    !order.zip ||
+    !order.phone ||
+    !order.cart ||
+    order.cart.length === 0
+  ) {
+    return res.status(400).send({ msg: "Invalid order data" });
+  }
+
+  order.date = new Date(); 
+
+  ordersCollection.insertOne(order, (err, result) => {
+    if (err) {
+      console.error("Error placing order:", err);
+      return res.status(500).send({ msg: "Failed to place order" });
+    }
+    res.send({ msg: "Order placed successfully", orderId: result.insertedId });
+  });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-
-    console.log(`Server is running on port: ${port}`);
-  });
+  console.log(`Server running on port ${port}`);
+});
